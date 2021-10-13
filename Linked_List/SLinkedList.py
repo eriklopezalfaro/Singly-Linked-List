e4534b8690cc984394e5937122b131083ebe566c
Erik Lopez <eriklopezalfaro@outlook.com>
2021-10-12 22:51:53 -0700

    Added more functionality to the linked list. Methods like add, insert at, delete. size, and clear list.

--- Linked_List/SLinkedList.py
+++ Linked_List/SLinkedList.py
@@ -17,6 +17,42 @@ class SLinkedList:
             curr = curr.next
             count += 1
 
+    def add_node(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        curr = self.head
+        while curr.next is not None:
+            curr = curr.next
+        curr.next = new_node
+
+    def delete_node(self, node):
+        if node == self.head:
+            self.head = node.next
+        curr = self.head.next
+        prev = self.head
+        while curr is not None:
+            if curr == node:
+                prev.next = curr.next
+            curr = curr.next
+            prev = prev.next
+
+    def size(self):
+        count = 0;
+        curr = self.head
+        while curr is not None:
+            count += 1
+        return count
+    def insert_at(self, new_data, prev):
+        new_node = Node(new_data)
+        if prev is None:
+            return f'Node doesnt exist. Linked list size({self.size()})'
+        new_node.next = prev.next
+        prev.next = new_node
+    
+    def clear_linked_list(self):
+        self.head = None  
+
 # create linkedlist and add nodes, print to console
 list1 = SLinkedList()
 list1.head = Node('Erik')
@@ -31,5 +67,17 @@ n2.next = n3
 n3.next = n4
 n4.next = n5
 
+n6 = Node('Csharon Baby girl')
+print('-------------------')
+list1.add_node('n6')
+list1.listprint()
+print('')
+list1.delete_node(n4)
 list1.listprint()
 
+print('')
+list1.insert_at('Joe',list1.head)
+list1.listprint()
+list1.insert_at('Seattle',n5)
+list1.listprint()
+print('-------------------')
